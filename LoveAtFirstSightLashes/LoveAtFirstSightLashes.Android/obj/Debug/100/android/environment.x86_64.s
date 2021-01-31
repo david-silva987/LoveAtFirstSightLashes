fdbecc1f518007d369d4b52c55b5f8f67ffb5627
David Da Silva <david.silva98@hotmail.fr>
2021-01-31 19:45:57 +0100

    change background color toolbar

--- LoveAtFirstSightLashes/LoveAtFirstSightLashes.Android/obj/Debug/100/android/environment.x86_64.s
+++ LoveAtFirstSightLashes/LoveAtFirstSightLashes.Android/obj/Debug/100/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"7ab2f20f-9dca-459f-9c76-87fd34936a59"
+	.asciz	"5ac0d95e-2e6e-4d60-a39f-450bd0b24487"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
